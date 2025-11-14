6449582ea391a28c0ca9b37922eac3d98f3f6671
Monit <monitbisht04@gmail.com>
2025-11-14 14:03:56 +0530

    Add Firebase and Firestore setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -54,6 +57,17 @@ dependencies {
     implementation(libs.circleimageview)
     implementation(libs.materialcalendarview)
     implementation(libs.mpandroidchart)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+
+
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // For Cloud Firestore (Database)
+    implementation("com.google.firebase:firebase-firestore")
+
 
 
 }
